2236be27c56ede48dfee8febf12bd2ab7a54207b
Thomas Habets <thomas@habets.se>
2020-05-27 11:16:00 +0100

    Switch contact fetching to the People Connections API

--- pkg/cmdg/contacts.go
+++ pkg/cmdg/contacts.go
@@ -6,15 +6,14 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	"sync"
-	"time"
 
 	log "github.com/sirupsen/logrus"
+	people "google.golang.org/api/people/v1"
 )
 
 const (
 	maxContacts      = 10000
-	contactBatchSize = 50
+	contactBatchSize = 2000
 )
 
 var (
@@ -50,77 +49,33 @@ func quoteNameIfNeeded(s string) string {
 
 // GetContacts gets all contact's email addresses in "Name Name <email@example.com>" format.
 func (c *CmdG) GetContacts(ctx context.Context) ([]string, error) {
-	// List contacts.
-	r, err := c.people.ContactGroups.Get("contactGroups/all").Context(ctx).MaxMembers(maxContacts).Do()
-	if err != nil {
-		return nil, err
-	}
-	log.Infof("Retrieved %d of %d contacts", len(r.MemberResourceNames), r.MemberCount)
-
-	// Get contact names/email addresses.
-	var wg sync.WaitGroup
-	pchan := make(chan string)
-	batches := len(r.MemberResourceNames)/contactBatchSize + 1
-	perr := make(chan error, batches)
-	for n := 0; ; n++ {
-		start := n * contactBatchSize
-		end := (n + 1) * contactBatchSize
-		if start >= len(r.MemberResourceNames) {
-			break
-		}
-		if end > len(r.MemberResourceNames) {
-			end = len(r.MemberResourceNames)
-		}
-		batch := r.MemberResourceNames[start:end]
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				p, err := c.people.People.GetBatchGet().Context(ctx).ResourceNames(batch...).PersonFields("names,emailAddresses").Do()
-				if err != nil {
-					log.Warningf("Error loading contacts: %v", err)
-					if strings.Contains(err.Error(), "quota") {
-						time.Sleep(time.Second)
-						continue
-					}
-					perr <- err
-					return
-				}
-				for _, r := range p.Responses {
-					// Use name first listed.
-					name := ""
-					if len(r.Person.Names) > 0 {
-						name = r.Person.Names[0].DisplayName
-					}
-					for _, e := range r.Person.EmailAddresses {
-						if strings.Contains(e.Value, " ") {
-							// Name already there.
-							log.Warningf("Contact email address contains a space: %q", e.Value)
-							pchan <- e.Value
-						} else {
-							if len(name) > 0 {
-								pchan <- fmt.Sprintf(`%s <%s>`, quoteNameIfNeeded(name), e.Value)
-							} else {
-								pchan <- e.Value
-							}
-						}
-					}
-				}
-				return
-			}
-		}()
-	}
-	go func() {
-		wg.Wait()
-		close(pchan)
-		close(perr)
-	}()
+	// TODO: get a sync token and only do incremental download.
 	var ret []string
-	for s := range pchan {
-		ret = append(ret, s)
-	}
-	for e := range perr {
-		return nil, e
+	if err := c.people.People.Connections.List("people/me").Context(ctx).PageSize(contactBatchSize).PersonFields("names,emailAddresses").Pages(ctx, func(r *people.ListConnectionsResponse) error {
+		log.Infof("Got batch of %d contacts, total %d", len(r.Connections), r.TotalItems)
+		for _, p := range r.Connections {
+			// Use name first listed.
+			var name string
+			if len(p.Names) > 0 {
+				name = p.Names[0].DisplayName
+			}
+			for _, e := range p.EmailAddresses {
+				if strings.Contains(e.Value, " ") {
+					// Name already there.
+					log.Warningf("Contact email address contains a space: %q", e.Value)
+					ret = append(ret, e.Value)
+				} else {
+					if len(name) > 0 {
+						ret = append(ret, fmt.Sprintf(`%s <%s>`, quoteNameIfNeeded(name), e.Value))
+					} else {
+						ret = append(ret, e.Value)
+					}
+				}
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
 	}
 	sort.Slice(ret, func(i, j int) bool {
 		return strings.TrimLeft(ret[i], `"`) < strings.TrimLeft(ret[j], `"`)
